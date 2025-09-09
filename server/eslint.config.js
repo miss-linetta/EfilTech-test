7315a23a155270794270d8054dde4fe42d26e0fe
miss-linetta <natashapetrovych@gmail.com>
2025-09-10 01:06:54 +0300

    refactor: delete unused files

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -1,53 +1,53 @@
-import js from '@eslint/js';
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
-import nextPlugin from '@next/eslint-plugin-next';
-import prettier from 'eslint-plugin-prettier';
-import importPlugin from 'eslint-plugin-import';
-import unusedImports from 'eslint-plugin-unused-imports';
-import nodePlugin from 'eslint-plugin-n';
-
-export default [
-  js.configs.recommended,
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      prettier,
-      import: importPlugin,
-      'unused-imports': unusedImports,
-      n: nodePlugin,
-      '@next/next': nextPlugin,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': 'off',
-      'unused-imports/no-unused-imports': 'error',
-      'unused-imports/no-unused-vars': [
-        'warn',
-        {
-          vars: 'all',
-          varsIgnorePattern: '^_',
-          args: 'after-used',
-          argsIgnorePattern: '^_',
-        },
-      ],
-
-      'import/order': [
-        'warn',
-        {
-          groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
-          'newlines-between': 'always',
-        },
-      ],
-      'n/no-missing-import': 'off',
-      'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    },
-  },
-];
+// import js from '@eslint/js';
+// import tsParser from '@typescript-eslint/parser';
+// import tsPlugin from '@typescript-eslint/eslint-plugin';
+// import nextPlugin from '@next/eslint-plugin-next';
+// import prettier from 'eslint-plugin-prettier';
+// import importPlugin from 'eslint-plugin-import';
+// import unusedImports from 'eslint-plugin-unused-imports';
+// import nodePlugin from 'eslint-plugin-n';
+//
+// export default [
+//   js.configs.recommended,
+//   {
+//     files: ['**/*.{ts,tsx}'],
+//     languageOptions: {
+//       parser: tsParser,
+//       parserOptions: {
+//         project: './tsconfig.json',
+//       },
+//     },
+//     plugins: {
+//       '@typescript-eslint': tsPlugin,
+//       prettier,
+//       import: importPlugin,
+//       'unused-imports': unusedImports,
+//       n: nodePlugin,
+//       '@next/next': nextPlugin,
+//     },
+//     rules: {
+//       'prettier/prettier': 'error',
+//       '@typescript-eslint/no-unused-vars': 'off',
+//       'unused-imports/no-unused-imports': 'error',
+//       'unused-imports/no-unused-vars': [
+//         'warn',
+//         {
+//           vars: 'all',
+//           varsIgnorePattern: '^_',
+//           args: 'after-used',
+//           argsIgnorePattern: '^_',
+//         },
+//       ],
+//
+//       'import/order': [
+//         'warn',
+//         {
+//           groups: [['builtin', 'external'], ['internal'], ['parent', 'sibling', 'index']],
+//           'newlines-between': 'always',
+//         },
+//       ],
+//       'n/no-missing-import': 'off',
+//       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+//     },
+//   },
+// ];
