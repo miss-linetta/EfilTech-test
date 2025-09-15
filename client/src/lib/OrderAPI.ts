4fc448b0412cf95968afa87af1054ce737084e49
miss-linetta <natashapetrovych@gmail.com>
2025-09-15 19:38:30 +0300

    fix: getOrderDetails error

--- client/src/lib/OrderAPI.ts
+++ client/src/lib/OrderAPI.ts
@@ -1,28 +1,33 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
+
 import { OrderData } from '@/lib/types';
+import { OrderDetails } from '@/components/pages/order-details-page/types';
 
 export const createOrder = async (orderData: OrderData) => {
   if (typeof window !== 'undefined') {
     console.log('Sending data:', orderData);
   }
   try {
-    const response = await axios.post(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/orders`,
-      orderData,
-      {
-        headers: { 'Content-Type': 'application/json' },
-      }
-    );
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/orders`, orderData, {
+      headers: { 'Content-Type': 'application/json' },
+    });
 
     if (typeof window !== 'undefined') {
       localStorage.removeItem('coupon_code');
     }
 
     return response.data;
-  } catch (error: any) {
+  } catch (error) {
     if (typeof window !== 'undefined') {
-      console.error('Error creating order:', error.response?.data || error.message);
+      console.error('Error creating order:', error);
     }
     return false;
   }
+};
+
+export const getOrderDetails = async (id: number): Promise<OrderDetails> => {
+  const response: AxiosResponse<OrderDetails> = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/orders/details/${id}`,
+  );
+  return response.data;
 };
